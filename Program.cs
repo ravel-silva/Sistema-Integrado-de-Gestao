ec3a62ad8565ee68456d97532d25bb85f4979b33
Ravel Mesquita <ravels78@gmail.com>
2025-06-03 21:18:41 -0300

    fix: habilita Swagger em produção

--- Program.cs
+++ Program.cs
@@ -72,11 +72,10 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseHttpsRedirection();
 
